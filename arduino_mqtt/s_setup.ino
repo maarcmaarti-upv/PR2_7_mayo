59558c8fed714e4f19d929ea59b35002580005e0
Marc Martí Ochaita <marcxeresa@gmail.com>
2026-05-07 11:56:27 +0200

    Add comments for variable declaration and subscriptions

--- arduino_mqtt/s_setup.ino
+++ arduino_mqtt/s_setup.ino
@@ -1,11 +1,12 @@
 void on_setup() {
-
+    // Declaración de variables
     const int sensor_caja = 4;
     const int sensor_paq = 5; 
     // Sensores
     pinMode(sensor_caja, INPUT_PULLUP);
     pinMode(sensor_paq, INPUT_PULLUP);
 
+    // Suscribirse a los topics
     suscribirseATopics();
     infoln("Sensores configurados en pines 4 y 5");
 
