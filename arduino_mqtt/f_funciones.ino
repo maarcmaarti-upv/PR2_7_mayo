02fc8cbffb022459b9f22fac8a71f6fac57b641c
Marc Martí Ochaita <marcxeresa@gmail.com>
2026-05-07 11:57:38 +0200

    Add comment about MQTT usage in setInternalLed
    Added a comment explaining the MQTT usage in the function.

--- arduino_mqtt/f_funciones.ino
+++ arduino_mqtt/f_funciones.ino
@@ -1,5 +1,5 @@
 uint8_t ledStatus = 0;
-
+// Aquí no hemos tocado nada ya que sólo usamos el MQTT para enviar mensajes a los topics dependiendo de los inputs que recibe la esp32s3
 void setInternalLed(uint8_t status) {
   if ( ledStatus == status ) // Nothing to do
     return;
